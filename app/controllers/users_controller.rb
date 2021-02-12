3465f443a366dec302cd354ed1236f7b32c4312f
wakametarou <60685052+wakametarou@users.noreply.github.com>
2021-02-13 08:24:48 +0900

    移植2

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      redirect_to user_path(id: current_user[:id])
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to(new_user_session_path)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
+    def set_user
+      @user = User.find_by(id: current_user[:id])
+    end
+end
